9f6434f43ad805533ea136b2740d8e1a06647730
Raphael Soares <raphaelsoares@pop-os.localdomain>
2020-07-29 20:27:39 -0300

    Form

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import Logo from '../../assets/img/logo.png';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.instagram.com/faelsoaresjf/?hl=pt-br" target="_blank">
+      <a href="https://www.instagram.com/faelsoaresjf/?hl=pt-br" target="_blank" rel="noopener noreferrer">
         <img src={Logo} alt="Logo Faelflix"/>
       </a>
       <p>
         Orgulhosamente criado por
         {' '}
-        <a href="https://www.instagram.com/faelsoaresjf/?hl=pt-br" target="_blank">
+        <a href="https://www.instagram.com/faelsoaresjf/?hl=pt-br" target="_blank" rel="noopener noreferrer">
           Raphael Soares xD
         </a>
       </p>
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TemplateBase from '../../../components/TemplateBase';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '#000000',
+    };
+
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+    
+
+    function handleChange(e) {
+        setValue(
+            e.target.getAttribute('name'), 
+            e.target.value
+        );
+    }
+   
+    function setValue(chave, valor){
+        setValues({
+            ...values,
+            [chave]: valor,
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setCategorias([...categorias, values]);
+        setValues(valoresIniciais);
+    }
+
     return (
         <TemplateBase>
-            <h1>Cadastro de Categoria</h1>
-            
+            <h1>Cadastro de Categoria: {values.nome}</h1>
+            <form onSubmit={handleSubmit}>
+
+                <FormField 
+                    label="Nome da Categoria"
+                    value={values.nome}
+                    onChange={handleChange}
+                    type="text"
+                    name="nome"
+                />
+                <div>
+                    <label>
+                        Descrição: 
+                        <textarea type="text" 
+                        name="descricao"
+                        value={values.descricao}
+                        onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <FormField 
+                    label="Cor"
+                    value={values.cor}
+                    onChange={handleChange}
+                    type="color"
+                    name="cor"
+                />
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria.nome}
+                        </li>
+                    )
+                })}
+            </ul>
             <Link to="/">
                 Home
             </Link>
